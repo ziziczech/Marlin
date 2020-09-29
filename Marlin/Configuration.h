14dd0b6df19a867fb7b0ca19ae712cef486a7f86
ziziczech <68967692+ziziczech@users.noreply.github.com>
2020-09-29 22:51:53 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "v200822-1"
+#define CUSTOM_MACHINE_NAME "v200902-1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
